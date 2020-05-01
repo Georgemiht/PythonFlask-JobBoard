526861133ea76acd26ece6df078d2488eff83e4d
Georgemiht <62032952+Georgemiht@users.noreply.github.com>
2020-05-01 10:01:43 -0400

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -5,6 +5,14 @@ PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
+def open_connection ():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection =g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
 
 @app.route('/')
 @app.route('/jobs')
