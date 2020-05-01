b9dce6fa54a5894a11ed6d63930b82855086cdda
Georgemiht <62032952+Georgemiht@users.noreply.github.com>
2020-05-01 02:02:27 -0400

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,9 +1,11 @@
 import sqlite3
 from flask import Flask, render_template, g
 
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
+def open_connection():
 @app.route('/')
 @app.route('/jobs')
 def jobs():
